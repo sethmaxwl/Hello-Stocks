d81490d55b8904ae92a7d6d12a404b4a82e193f1
Seth <sethmaxwl@gmail.com>
2018-09-15 15:09:03 -0400

    Searching for symbol outputs JSON to console

--- app/components/lookup-stock.js
+++ app/components/lookup-stock.js
@@ -3,10 +3,11 @@ import $ from 'jquery';
 
 export default Component.extend({
   searchSuccess: true,
-  stockSearch: '',
+  stockSearch: "",
+  graphOptions: {},
   actions:{
     search(){
-      let searchURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + stockSearch + "&output=compact&apikey=F3XPPAPBQJU1RE34";
+      let searchURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + this.get("stockSearch") + "&interval=5min&apikey=F3XPPAPBQJU1RE34";
       $.getJSON(searchURL, function(data){
         console.log(data);
       });
